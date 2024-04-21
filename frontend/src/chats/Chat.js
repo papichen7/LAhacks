227a18647b36e1ac91586d21a2ffe61841fa800e
Peter Lee <chiwoonlee@gmail.com>
2024-04-21 00:10:07 -0700

    popup code

--- frontend/src/chats/Chat.js
+++ frontend/src/chats/Chat.js
@@ -6,7 +6,6 @@ function Chat({ name, image, handler, id, users }) {
                 <img src={image} alt="" className="h-14 w-14 rounded-[32px]"></img>
                 <div className="mx-2 mt-1">
                     <button onClick={() => handler(id)}>
-
                         <p className="font-semibold">{name}</p>
                         <p className="text-xs">Latest Message</p>
                     </button>
